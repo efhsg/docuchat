47e5bac2b293aacc11de27f81bae439afd843d3f
esg <efhsg@live.nl>
2024-02-06 00:21:02 +0100

    Textual changes

--- src/Main.py
+++ src/Main.py
@@ -3,19 +3,51 @@ from PIL import Image
 
 
 def home_page():
-
     image = Image.open("./img/logo_small.png")
     st.set_page_config(
-        page_title="Docuchat home",
+        page_title="DocuChat Home",
         page_icon=image,
         layout="wide",
         initial_sidebar_state="auto",
     )
 
-    st.image("./img/logo_small.png")
+    col1, col2 = st.columns([1, 1])
 
-    st.title("DocuChat")
-    st.write("Upload, embed and chat with multiple files")
+    with col1:
+        st.title("Welcome to DocuChat!")
+        st.write(
+            """
+            DocuChat enhances your document collaboration experience.
+            Upload, embed, and chat with multiple files seamlessly.
+            """
+        )
+
+    with col2:
+        st.image("./img/logo_small.png", width=150)  # Adjust width as needed
+
+    st.markdown("---")
+    st.header("How DocuChat Works")
+
+    st.markdown(
+        """
+        **Step 1: Upload Files and Extract Text**
+        - You can upload multiple PDF files.
+        - DocuChat automatically extracts text from these files for processing.
+
+        **Step 2: Select and Combine Extracted Text**
+        - Extracted text is then selected and combined into a knowledge base.
+        - This process involves vector embedding to facilitate efficient information retrieval.
+
+        **Step 3: Chat with Your Knowledge Base**
+        - Utilize your favorite Large Language Model (LLM) to chat with the created knowledge base.
+        - Ask questions or seek information directly from the content of your uploaded documents.
+        """
+    )
+
+    st.markdown("---")
+    st.write(
+        "Ready to get started? Upload your files and explore the capabilities of DocuChat."
+    )
 
 
 if __name__ == "__main__":
