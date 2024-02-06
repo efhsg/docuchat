d6bd401cb4d4b2c22f373eab1075c6741758160c
esg <efhsg@live.nl>
2024-02-06 22:08:23 +0100

    Unit test added

--- tests/unit/test_Extract_text.py
+++ tests/unit/test_Extract_text.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from Extract_text import get_pdf_file_path
+from Extract_text import get_pdf_file_path, get_text_file_path
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,3 +16,13 @@ class GetPdfFilePathTest(unittest.TestCase):
 
         result_path = get_pdf_file_path(file_name)
         self.assertEqual(expected_path, result_path)
+
+
+class GetTextFilePathTest(unittest.TestCase):
+    @patch("Extract_text.Config")
+    def test_get_text_file_path(self, mock_config):
+        mock_config.TEXT_DIR = "/fake/text/dir"
+        file_name = "document.pdf"
+        expected_text_file_path = "/fake/text/dir/document.txt"
+        result_path = get_text_file_path(file_name)
+        self.assertEqual(expected_text_file_path, result_path)
