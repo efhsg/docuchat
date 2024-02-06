49d25dc5de0c424f94a1d5d4b2ed2c536532fd5d
esg <efhsg@live.nl>
2024-02-06 21:57:15 +0100

    Refactoring and first unittest

--- src/Config.py
+++ src/Config.py
@@ -1,7 +1,11 @@
 import os
+from pathlib import Path
 
 
 class Config:
     UPLOAD_EXTENSIONS = ("pdf", "txt")
     UPLOAD_DIR = os.getenv("UPLOAD_DIR", "upload")
     TEXT_DIR = os.getenv("TEXT_DIR", UPLOAD_DIR + "/extracted_text")
+
+    current_file_path = Path(__file__).resolve()
+    logo_small_path = str(current_file_path.parent / "img/logo_small.png")
--- tests/integration/pages/test_10_Extract_text.py
+++ tests/integration/pages/test_10_Extract_text.py
@@ -5,6 +5,5 @@ logger = logging.getLogger(__name__)
 
 
 def test_home_page():
-    at = AppTest.from_file("./src/Main.py").run()
+    at = AppTest.from_file("./src/pages/10_Extract_text.py").run()
     assert not at.exception
-    assert at.title[0].value == "Welcome to DocuChat!"
