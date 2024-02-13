8d94dd374f90f3502824286eef59065bc02a3a3b
esg <efhsg@live.nl>
2024-02-13 15:21:23 +0100

    moved ambelic and Dockerfile

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -11,7 +11,6 @@ from pathlib import Path
 project_root = Path(__file__).resolve().parents[1]
 sys.path.append(str(project_root / "src"))
 
-# Import your Config class
 from config import Config
 
 
