b0a58aa8b0e9fd75728ce0a339ce5d0fb7f38422
esg <efhsg@live.nl>
2024-02-19 14:07:18 +0100

    Refactoring

--- src/injector.py
+++ src/injector.py
@@ -1,4 +1,3 @@
-from components.database.interfaces.connector import Connector
 from components.database.mysql_connector import MySQLConnector
 from components.reader.sqlAlchemy_reader_repository import SqlalchemyReaderRepository
 from components.reader.interfaces.reader_repository import ReaderRepository
@@ -6,14 +5,13 @@ from components.reader.zlib_text_compressor import ZlibTextCompressor
 from components.reader.interfaces.text_extractor import TextExtractor
 from components.reader.file_text_extractor import FileTextExtractor
 from components.logger.native_logger import NativeLogger
-from sqlalchemy.orm import Session
 
 
 def get_reader_repository() -> ReaderRepository:
-    connector: Connector = MySQLConnector()
-    logger = NativeLogger.get_logger("docuchat")
     return SqlalchemyReaderRepository(
-        connector=connector, compressor=ZlibTextCompressor(), logger=logger
+        connector=MySQLConnector(),
+        compressor=ZlibTextCompressor(),
+        logger=NativeLogger.get_logger("docuchat"),
     )
 
 
