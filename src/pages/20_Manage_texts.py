d736ddc16356162f16d893109f06e9f234ae71a8
esg <efhsg@live.nl>
2024-02-23 02:36:20 +0100

    Refactoring and error handling

--- src/pages/20_Manage_texts.py
+++ src/pages/20_Manage_texts.py
@@ -28,24 +28,12 @@ def select_domain():
 
 def manage_texts(selected_domain):
     st.title(selected_domain)
-    text_options = reader_repository.list_text_names_by_domain(selected_domain)
-    selected_text = select_text(text_options)
+    selected_text = select_text(
+        reader_repository.list_text_names_by_domain(selected_domain)
+    )
     if selected_text:
         handle_text_renaming(selected_domain, selected_text)
         display_text_content(selected_domain, selected_text)
-
-
-def handle_text_renaming(selected_domain, selected_text):
-    with st.form("Rename_text", clear_on_submit=False):
-        new_name = st.text_input("Rename text", value=selected_text)
-        save_text_name = st.form_submit_button(
-            label="Save",
-        )
-
-    if save_text_name:
-        if reader_repository.update_text_name(selected_text, new_name, selected_domain):
-            st.session_state.update(context_text=new_name)
-            st.rerun()
 
 
 def select_text(text_options):
@@ -62,9 +50,31 @@ def select_text(text_options):
     return selected_text
 
 
+def handle_text_renaming(selected_domain, selected_text):
+    with st.form("Rename_text", clear_on_submit=False):
+        new_name = st.text_input("Rename text", value=selected_text)
+        save_text_name = st.form_submit_button(
+            label="Save",
+        )
+
+    if save_text_name:
+        if reader_repository.update_text_name(selected_text, new_name, selected_domain):
+            st.session_state.update(context_text=new_name)
+            st.rerun()
+
+
 def display_text_content(selected_domain, selected_text):
-    text_content = reader_repository.get_text_by_name(selected_text, selected_domain)
-    st.text_area("content", value=text_content, height=400, disabled=True)
+    try:
+        text_content = reader_repository.get_text_by_name(
+            selected_text, selected_domain
+        )
+        if text_content is not None:
+            st.text_area("content", value=text_content, height=400, disabled=True)
+        else:
+            st.error("Text content not found.")
+    except Exception as e:
+        logger.error(f"Error displaying text content: {e}")
+        st.error(f"An error occurred while displaying the text content: {e}")
 
 
 def main():
