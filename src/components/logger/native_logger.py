4d0776f5b9910f668ee509290f2a34862644f203
esg <efhsg@live.nl>
2024-03-10 17:21:25 +0100

    refactored natvive logger, exit Milvus

--- src/components/logger/native_logger.py
+++ src/components/logger/native_logger.py
@@ -1,39 +1,53 @@
-from .interfaces.logger import Logger
-import logging
-from logging import Logger as StandardLogger
+from configparser import ConfigParser
+from io import StringIO
+from logging import Logger
 import logging.config
 from pathlib import Path
 from shutil import copyfile
+import re
 
 
 class NativeLogger(Logger):
     _logger = None
-    _base_dir = Path(__file__).resolve().parent
-    _example_config_path = _base_dir / "logging.example.ini"
-    config_filename = _base_dir / "logging.ini"
+    _example_config_file = Path(__file__).resolve().parent / "logging.example.ini"
+    _root_dir = Path(__file__).resolve().parents[3]
+    _config_file = _root_dir / "logging.ini"
 
     @classmethod
-    def _get_config_path(cls):
-        return cls._base_dir / cls.config_filename
+    def _prepare_config_file(cls):
+        if not cls._config_file.exists():
+            copyfile(cls._example_config_file, cls._config_file)
 
     @classmethod
-    def _prepare_config_file(cls, config_path):
-        if not config_path.exists():
-            copyfile(cls._example_config_path, config_path)
+    def _adjust_config_paths(cls, config_str):
+        def replacer(match):
+            original_path = match.group(1)
+            new_path = cls._root_dir / original_path
+            return f"args=('{new_path}', 'a')"
+
+        pattern = r"args=\('\s*([^']+)',\s*'a'\s*\)"
+        adjusted_config_str = re.sub(pattern, replacer, config_str)
+
+        return adjusted_config_str
 
     @classmethod
-    def _load_config(cls, config_path):
-        logging.config.fileConfig(str(config_path), disable_existing_loggers=False)
+    def _load_config(cls):
+        with open(cls._config_file, "r") as f:
+            config_str = f.read()
+
+        adjusted_config_str = cls._adjust_config_paths(config_str)
+
+        config_stream = StringIO(adjusted_config_str)
+        logging.config.fileConfig(config_stream, disable_existing_loggers=False)
 
     @classmethod
     def _ensure_configured(cls):
         if cls._logger is None:
-            config_path = cls._get_config_path()
-            cls._prepare_config_file(config_path)
-            cls._load_config(config_path)
+            cls._prepare_config_file()
+            cls._load_config()
             cls._logger = True
 
-    @staticmethod
-    def get_logger(name="docuchat") -> StandardLogger:
-        NativeLogger._ensure_configured()
+    @classmethod
+    def get_logger(cls, name="docuchat") -> logging.Logger:
+        cls._ensure_configured()
         return logging.getLogger(name)
